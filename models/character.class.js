9727e4214524d3bb978f84ca75441f9373a3d010
user7d11 <user7w11@gmail.com>
2022-07-06 15:26:10 +0200

    character animate

--- models/character.class.js
+++ models/character.class.js
@@ -2,16 +2,31 @@ class Character extends MovableObject {
     height = 280;
     y = 155;
 
+    ImagesWalking = [
+        './img/2_character_pepe/2_walk/W-21.png',
+        './img/2_character_pepe/2_walk/W-22.png',
+        './img/2_character_pepe/2_walk/W-23.png',
+        './img/2_character_pepe/2_walk/W-24.png',
+        './img/2_character_pepe/2_walk/W-25.png',
+        './img/2_character_pepe/2_walk/W-26.png'
+    ];
+
+    currentImage = 0;
+
     constructor() {
         super().loadImage('./img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages([
-            './img/2_character_pepe/2_walk/W-21.png',
-            './img/2_character_pepe/2_walk/W-22.png',
-            './img/2_character_pepe/2_walk/W-23.png',
-            './img/2_character_pepe/2_walk/W-24.png',
-            './img/2_character_pepe/2_walk/W-25.png',
-            './img/2_character_pepe/2_walk/W-26.png',
-        ]);
+        this.loadImages(this.ImagesWalking);
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.ImagesWalking.length; // modulo - schleiffe der länge
+            let path = this.ImagesWalking[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);
     }
 
     jump() {
