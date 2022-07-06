9c7af8b0e32118eb8e3b70e35949eb3991dfce97
user7d11 <user7w11@gmail.com>
2022-07-06 12:27:09 +0200

    background and pics

--- models/world.class.js
+++ models/world.class.js
@@ -1,14 +1,20 @@
 class World {
 
     character = new Character();
+
     enemies = [
         new Chiken(),
         new Chiken(),
         new Chiken()
     ];
+
     clouds = [
         new Cloud()
     ];
+
+    backgroundObjects = [
+        new BackgroundObject('./img/5_background/layers/3_third_layer/1.png', 0, 75)
+    ]
 
     canvas;
     ctx;
@@ -21,19 +27,27 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
 
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-        });
+        this.addToMap(this.character);
 
-        this.clouds.forEach(cloud => {
-            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-        });
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.backgroundObjects);
 
         let self = this;
         requestAnimationFrame(function () {
             self.draw();
         });
     }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o)
+        });
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+    }
+
 }
