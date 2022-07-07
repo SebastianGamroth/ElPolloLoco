3a1ffdc704ce87a42908be24395b19a1136bb1f0
user7d11 <user7w11@gmail.com>
2022-07-07 12:35:08 +0200

    Collision detection

--- models/movableObject.class.js
+++ models/movableObject.class.js
@@ -51,6 +51,13 @@ class MovableObject {
         }
     }
 
+    isColliding(mo) {
+        return this.x + this.width > mo.x &&
+            this.y + this.height > mo.y &&
+            this.x < mo.x &&
+            this.y < mo.y + mo.height;
+    }
+
     playAnimation(images) {
         let i = this.currentImage % this.ImagesWalking.length; // modulo - schleiffe der länge
         let path = images[i];
