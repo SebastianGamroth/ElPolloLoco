3a783123b886dbf4cb47e92af6b7837d6aaf2e82
user7d11 <user7w11@gmail.com>
2022-07-07 10:50:01 +0200

    Pepe should jump

--- models/character.class.js
+++ models/character.class.js
@@ -51,6 +51,10 @@ class Character extends MovableObject {
                 this.walkingSound.play();
             }
 
+            if (this.world.keyboard.UP) {
+                this.speedY = 20;
+            }
+
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
 
