525f13ec09a4a64fe2f7912c875fb9eb1316501a
user7d11 <user7w11@gmail.com>
2022-07-07 11:55:51 +0200

    optimize code

--- models/movableObject.class.js
+++ models/movableObject.class.js
@@ -37,6 +37,18 @@ class MovableObject {
         });
     }
 
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+        ctx.beginPath();
+        ctx.lineWidth = '5';
+        ctx.strokeStyle = 'blue';
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+    }
+
     playAnimation(images) {
         let i = this.currentImage % this.ImagesWalking.length; // modulo - schleiffe der länge
         let path = images[i];
